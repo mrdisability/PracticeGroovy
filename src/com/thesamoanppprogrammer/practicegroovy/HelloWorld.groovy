4757d2fd8a709af6110622200eee7f02014ec567
King Ioane <thesamoanppprogrammer@gmail.com>
2022-02-09 10:29:44 +1300

    Initial commit

---
+++ src/com/thesamoanppprogrammer/practicegroovy/HelloWorld.groovy
@@ -0,0 +1,7 @@
+package com.thesamoanppprogrammer.practicegroovy
+
+class HelloWorld {
+    public static void main(String[] args) {
+        println("Hello World")
+    }
+}
---
+++ src/com/thesamoanppprogrammer/practicegroovy/xml.groovy
@@ -0,0 +1,18 @@
+package com.thesamoanppprogrammer.practicegroovy
+
+import groovy.xml.MarkupBuilder
+
+MarkupBuilder markupBuilder = new MarkupBuilder()
+
+markupBuilder.sports {
+    sport(id:1) {
+        name: 'Rugby'
+    }
+    sport(id:2) {
+        name: 'Soccer'
+    }
+    sport(id:3) {
+        name: 'Golf'
+    }
+}
+
