3c5504fb60cd6865cae024eec1ec8728455bd6d1
King Ioane <thesamoanppprogrammer@gmail.com>
2022-02-10 22:22:08 +1300

    Fixed error by downgrading Groovy to 3.0.9 and added todos get request

--- src/com/thesamoanppprogrammer/practicegroovy/restapi/httpbuilder/todos.groovy
+++ src/com/thesamoanppprogrammer/practicegroovy/restapi/httpbuilder/todos.groovy
@@ -3,13 +3,24 @@ package com.thesamoanppprogrammer.practicegroovy.restapi.httpbuilder
 import groovyx.net.http.RESTClient
 import groovyx.net.http.ContentType
 
-String base = 'http://api.icndb.com'
+//String base = 'http://api.icndb.com'
+//
+//def chuck = new RESTClient(base)
+//def params = [firstName: "Dan", lastName: "Vega"]
+//
+//chuck.contentType = ContentType.JSON
+//chuck.get( path: '/jokes/random', query: params ) { response, json ->
+//    println response.status
+//    println json
+//}
 
-def chuck = new RESTClient(base)
-def params = [firstName: "Dan", lastName: "Vega"]
+String base = 'https://61e281e83050a100176821ca.mockapi.io'
 
-chuck.contentType = ContentType.JSON
-chuck.get( path: '/jokes/random', query: params ) { response, json ->
+def client = new RESTClient(base)
+//def params = [firstName: "Dan", lastName: "Vega"]
+
+client.contentType = ContentType.JSON
+client.get( path: '/todos') { response, json ->
     println response.status
     println json
 }
