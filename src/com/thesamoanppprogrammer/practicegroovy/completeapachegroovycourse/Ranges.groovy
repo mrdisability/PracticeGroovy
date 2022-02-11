f4c3c2c7013128c9123c6f357d1d02b3398b2f43
King Ioane <thesamoanppprogrammer@gmail.com>
2022-02-11 14:19:59 +1300

    Added collections, lists and ranges

---
+++ src/com/thesamoanppprogrammer/practicegroovy/completeapachegroovycourse/Ranges.groovy
@@ -0,0 +1,21 @@
+package com.thesamoanppprogrammer.practicegroovy.completeapachegroovycourse
+
+enum Days {
+    SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY
+}
+
+def dayRange = Days.SUNDAY..Days.SATURDAY
+
+dayRange.each { day ->
+    println day
+}
+
+println dayRange.size()
+println dayRange.contains(Days.WEDNESDAY)
+
+// Bonus: next() and previous() are equivalent to
+// ++ and -- operators.
+def wednesday = Days.WEDNESDAY
+assert Days.THURSDAY == ++wednesday
+assert Days.WEDNESDAY == --wednesday
+
